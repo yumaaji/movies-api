a8a19a44dddf177462e0a65cd07cd80651ade321
yumaaji <yumaaji126@gmail.com>
2024-07-16 22:01:23 +0700

    modifikasi card

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,62 @@
-import './App.css'
-import { useState } from 'react'
-import { useEffect} from 'react'
-import { getMovies, searchMovie } from './api';
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import { getMovies, searchMovie } from "./api";
 
 const App = () => {
-  const [popularMovies, setPopularMovies] = useState([])
+  const [popularMovies, setPopularMovies] = useState([]);
 
   useEffect(() => {
     getMovies().then((resultData) => {
-      setPopularMovies(resultData)
-    })
-  }, [])
+      setPopularMovies(resultData);
+    });
+  }, []);
 
   // Mapping data movies from API
   const popularMoviesList = () => {
     return popularMovies.map((movie, i) => {
-      return(
-        <div className="bg-slate-400 h-auto d-flex flex-col mb-3 rounded" key={i} style={{width: '250px'}}>
-          <div className="movie-title my-3 text-xs font-bold">{movie.original_title}</div>
-          <img className="" src={`${import.meta.env.VITE_APP_BASEIMAGEURL}${movie.poster_path}`} style={{width: '250px', height: '300px'}}/>
-          <div className="movie-date mt-3 italic">Release: {movie.release_date}</div>
-          <div className="movie-rate mb-4 text-slate-600 font-bold underline">Rate: {movie.vote_average}</div>
-        </div>
-      )
-    })
-  }
+      return (
+        <>
+          <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg" key={i} style={{ width: "270px" }}>
+            <img className="" src={`${import.meta.env.VITE_APP_BASEIMAGEURL}${movie.poster_path}`} style={{ width: "270px", height: "340px" }} />
+            <div className="bg-white p-4 sm:p-6">
+              <h3 className="mt-0.5 text-xl font-bold text-gray-900">{movie.original_title}</h3>
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 mb-2">{movie.overview}</p>
+              <hr />
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">Release date: {movie.release_date}</p>
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-500 italic">Vote Average: {movie.vote_average.toFixed(1)}</p>
+            </div>
+          </article>
+        </>
+      );
+    });
+  };
 
   // Search movies
   const search = async (q) => {
-    if (q.length > 3){
+    if (q.length > 3) {
       // send paramater to API
-      const query = await searchMovie(q)
+      const query = await searchMovie(q);
       // set state
-      setPopularMovies(query.results)
+      setPopularMovies(query.results);
     }
-  }
+  };
 
   // Render result
   return (
     <>
-    <div className="container flex flex-col justify-center items-center">
-      <div className="flex flex-col justify-center items-center">
-        <h1 className='text-3xl font-bold mb-3 bg-slate-400 p-3 rounded text-white'>Movies List</h1>
-        <h6 className='text-sm font-bold text-black italic mb-3 opacity-25'>API by: The Movie Database</h6>
-        <input type="text" placeholder='Search movie name here' autoFocus className='w-2/3 h-16 bg-slate-400 text-center placeholder-white rounded-md mb-10' onChange={({target}) => search(target.value)}/>
-        <div className='flex justify-center flex-wrap gap-10'>
-          {popularMoviesList()}
+      <div className="container flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-3xl font-bold mb-3 bg-slate-100 py-3 px-5 rounded text-slate-300">Movies List</h1>
+          <div className="w-3/4 mb-10">
+            <input type="text" id="Search" placeholder="Search movie name..." className="w-full h-16 rounded-md border-gray-400 py-2.5 shadow-md sm:text-sm text-center" onChange={({ target }) => search(target.value)}/>
+          </div>
+          <div className="flex justify-center flex-wrap gap-10">{popularMoviesList()}</div>
+          <h6 className="text-sm font-bold text-black italic my-10 opacity-25">API by: The Movie Database</h6>
         </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
